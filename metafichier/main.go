da550d389f46b96e979b8b2b4287a9eb5259683d
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2020-08-02 08:20:51 +0100

    Extraction des informations du metafichier

--- metafichier/main.go
+++ metafichier/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -29,19 +30,11 @@ func intToByteArray(number int) []byte {
 	return x
 }
 
-func main() {
-
-	now := time.Now()
-	msec := now.Unix()
-
+func buildMetafichier(filename string) {
 	var version int = 1
 	var taille int = 0
 	var nbu int = 0
-	var filename string = os.Args[0]
-
-	if len(os.Args) > 1 {
-		filename = os.Args[1]
-	}
+	var sep []byte = []byte{31}
 
 	fmt.Println("Génération du métafichier ...")
 
@@ -86,27 +79,24 @@ func main() {
 	check(e)
 
 	// Écriture du hash
-	_, e = m.WriteString(":")
-	check(e)
 	_, errHash := m.Write(hash)
 	check(errHash)
 
 	// Écriture du nombre de bits à 1
-	_, e = m.WriteString(":")
 	check(e)
 	_, e = m.Write(intToByteArray(nbu))
 	check(e)
 
-	// Écriture de la taille du fichier
-	_, e = m.WriteString(":")
-	check(e)
-	_, e = m.Write(intToByteArray(taille))
-	check(e)
-
 	// Écriture du nom du fichier
-	_, e = m.WriteString(":")
+	_, e = m.Write(sep)
 	check(e)
 	_, e = m.WriteString(filename)
+	check(e)
+
+	// Écriture de la taille du fichier
+	_, e = m.Write(sep)
+	check(e)
+	_, e = m.Write(intToByteArray(taille))
 	check(e)
 
 	m.Sync()
@@ -118,6 +108,73 @@ func main() {
 	fmt.Printf("     nbu: %d (0x%x)\n", nbu, nbu)
 	fmt.Printf("  taille: %d (0x%x)\n", taille, taille)
 	fmt.Println("     nom:", filename)
+}
+
+func extractData(filename string) {
+
+	fmt.Println("Lecture du métafichier ...")
+
+	// Ouverture du metafichier
+	m, e := os.Open(filename)
+	check(e)
+
+	r := bufio.NewReader(m)
+
+	bVersion, e := r.ReadByte()
+	check(e)
+
+	// Récupération de la version utilisée de reco
+	version := bVersion
+
+	// Récupération du hash
+	bHash := make([]byte, 20)
+	_, e = r.Read(bHash)
+	check(e)
+
+	// Récupération du nombre de bits à 1
+	bNBU, e := r.ReadBytes(31)
+	check(e)
+	bNBU = bNBU[:len(bNBU)-1]
+	nbu, e := strconv.ParseUint(hex.EncodeToString(bNBU), 16, 32)
+	check(e)
+
+	// Récupération du nom du fichier
+	bNom, e := r.ReadBytes(31)
+	bNom = bNom[:len(bNom)-1]
+	check(e)
+
+	// Récupération de la taille du fichier
+	bTaille, e := r.ReadBytes(31)
+	if e != io.EOF {
+		check(e)
+	}
+	taille, e := strconv.ParseUint(hex.EncodeToString(bTaille), 16, 32)
+	check(e)
+
+	fmt.Printf("\n version: %x\n", version)
+	fmt.Printf("   SHA-1: %s\n", hex.EncodeToString(bHash))
+	fmt.Printf("     nbu: %d\n", nbu)
+	fmt.Printf("  taille: %d\n", taille)
+	fmt.Printf("     nom: %s\n", bNom)
+}
+
+func main() {
+
+	now := time.Now()
+	msec := now.Unix()
+
+	var filename string = os.Args[0]
+
+	if len(os.Args) == 3 {
+		param := os.Args[1]
+		if param == "-m" {
+			metafichier := os.Args[2]
+			extractData(metafichier)
+		}
+	} else if len(os.Args) == 2 {
+		filename = os.Args[1]
+		buildMetafichier(filename)
+	}
 
 	fmt.Println("\nTemps d'exécution:", (time.Now().Unix() - msec), "s")
 }
